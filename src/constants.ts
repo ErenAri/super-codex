4d849e750f6efae2ef1fb0a56014fa8c6cab1d48
ErenAri <erenari27@gmail.com>
2026-03-02 10:26:20 +0300

    chore: bump version to 1.1.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const SUPERCODEX_VERSION = "1.0.0";
+export const SUPERCODEX_VERSION = "1.1.0";
 export const SUPERCODEX_PROMPT_PACK = "supercodex";
 export const SUPERCODEX_DEFAULT_AGENT = "supercodex_planner";
